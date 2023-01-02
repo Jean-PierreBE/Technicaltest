017016770f1727f7c75fb00416678c685fed46c7
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2023-01-02 21:55:13 +0100

    ier commit

--- functions/function_to_test.py
+++ functions/function_to_test.py
@@ -8,14 +8,9 @@ def parse_to_roman(num_arabe):
     elif(num_arabe < 0):
         print("nombre négatif")
     else:
-        while num_arabe >= arabe[0]:
-            result = result + roman[0]
-            num_arabe = num_arabe - arabe[0]
-        while num_arabe >= arabe[1]:
-            result = result + roman[1]
-            num_arabe = num_arabe - arabe[1]
-        while num_arabe >= arabe[2]:
-            result = result + roman[2]
-            num_arabe = num_arabe - arabe[2]
+        for ind in range(len(arabe)):
+            while num_arabe >= arabe[ind]:
+                result = result + roman[ind]
+                num_arabe = num_arabe - arabe[ind]
 
         print(result)
